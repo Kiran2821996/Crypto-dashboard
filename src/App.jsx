02f72893f55c818e7edca38622571b10ffcb9d99
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-11-09 13:10:01 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import  Search  from './Components/MenuAppBar'
+import MainPage from './Components/MainPage'
+export default function App() {
+  return (
+//    <BrowserRouter>
+   <div>
+    <MainPage/>
+   </div>
+   
+//    </BrowserRouter>
+  )
+}
---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuAppBar from './MenuAppBar'
+export default function MainPage() {
+  return (
+   <>
+   <MenuAppBar/>
+   <>
+   
+   </>
+   </>
+  )
+}
